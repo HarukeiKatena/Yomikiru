85019d8e5c9365002e8008910bf62188f4bcc8ab
harukeiNote <58187672+hakiyoshi@users.noreply.github.com>
2022-06-14 17:14:24 +0900

    フォーマット整理

--- YomikiruSecond/Assets/Scripts/GameSequence/EndSequence.cs
+++ YomikiruSecond/Assets/Scripts/GameSequence/EndSequence.cs
@@ -1,54 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
 using Cinemachine;
 using Cysharp.Threading.Tasks;
 using Yomikiru.Enemy;
 using Player;
 using UnityEngine;
 using UniRx;
-using UnityEngine.SceneManagement;
 using Yomikiru;
 
 public class EndSequence : MonoBehaviour
 {
     [SerializeField] private MatchInfo matchInfo;
-
-    [SerializeField]
-    protected PlayerManagement playerManagement;
-
-    [SerializeField]
-    protected CinemachineVirtualCamera endTargetVirtualCamera;
-
-    [SerializeField]
-    protected Yomikiru.UI.EndDisplay display;
-
-    [SerializeField]
-    protected float CameraMoveDelay = 1.0f;
-
-    [SerializeField]
-    protected float EndWaitTime = 2.0f;
+    [SerializeField] private PlayerManagement playerManagement;
+    [SerializeField] private CinemachineVirtualCamera endTargetVirtualCamera;
+    [SerializeField] private Yomikiru.UI.EndDisplay display;
+    [SerializeField] private float cameraMoveDelay = 1.0f;
+    [SerializeField] private float endWaitTime = 2.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         foreach (var c in playerManagement.Character)
         {
-            if(c.TryGetComponent<Die>(out var charactor)){
+            if(c.TryGetComponent<Die>(out var charactor))
                 charactor.DieIvent.Subscribe(x => { StartCoroutine(SceneChange(x)); }).AddTo(this);
-            }
-            else if(c.TryGetComponent<AIEnemyBase>(out var enemy)) {
+            else if(c.TryGetComponent<AIEnemyBase>(out var enemy))
                 enemy.DieEvent.Subscribe(x => { StartCoroutine(SceneChange(x)); }).AddTo(this);
-            }
         }
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     IEnumerator SceneChange(int playerindex)
     {
@@ -56,10 +34,10 @@ public class EndSequence : MonoBehaviour
 
         display.DisplayAsync().Forget();
 
-        yield return new WaitForSeconds(CameraMoveDelay);
+        yield return new WaitForSeconds(cameraMoveDelay);
         endTargetVirtualCamera.Priority = 20;
 
-        yield return new WaitForSeconds(EndWaitTime);
+        yield return new WaitForSeconds(endWaitTime);
 
         //CheckControllerでやったらうまく動かなかったからとりあえずこっちに書く
         ControllerManagement.ClearPlayerDevice();
