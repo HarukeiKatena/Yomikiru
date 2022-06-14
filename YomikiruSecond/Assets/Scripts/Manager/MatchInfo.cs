6e7710ab5606980e8b9a5e28982e837db9a06abf
i-yuuki <i-yuuki@users.noreply.github.com>
2022-06-14 17:27:28 +0900

    MatchInfoの[Tooltip]を消す
    Inspectorからの編集は想定していません (ｷﾘｯ
    ていうか普通に情報量ないからいらない

--- YomikiruSecond/Assets/Scripts/Manager/MatchInfo.cs
+++ YomikiruSecond/Assets/Scripts/Manager/MatchInfo.cs
@@ -17,7 +17,6 @@ public class MatchInfo : ScriptableObject
         set => state.Value = value;
     }
 
-    [Tooltip("勝者のインデックス 0～")]
     public int WinnerPlayerIndex = -1;
 
     public IObservable<MatchState> OnStateChange => state;
