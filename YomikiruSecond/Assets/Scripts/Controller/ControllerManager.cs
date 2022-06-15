08f9817c027b454337c6d912f70de560ed29080f
harukeiNote <58187672+hakiyoshi@users.noreply.github.com>
2022-06-15 11:16:12 +0900

    コントローラーマネージャーをScriptableObjectにして作り直した

---
+++ YomikiruSecond/Assets/Scripts/Controller/ControllerManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Yomikiru.Controller
+{
+    [CreateAssetMenu(menuName = "ScriptableObjects/ControllerManager")]
+    public class ControllerManager : ScriptableObject
+    {
+        public const int MaxPlayerCound = 2;
+
+        public int PlayerCount = 2;
+        public Gamepad[] PlayerDevices = new Gamepad[2];
+
+        public const int NotUsedKeybord = -1; //未使用時の値
+
+        //キーボードを利用してるプレイヤーインデックス(使わない場合-1)
+        public int KeybordPlayerIndex = NotUsedKeybord;
+
+        //プレイヤー数の設定
+        public void SetOnePlayer()
+            => PlayerCount = 1;
+
+        public void SetTwoPlayer()
+            => PlayerCount = 2;
+
+        // 全てのプレイヤー分だけのパッドまたはキーボードが登録されているか
+        public bool IsControllersForAllPlayersSet()
+        {
+            for (int i = 0; i < PlayerCount; i++)
+            {
+                var gamepad = PlayerDevices[i];
+                if (gamepad == null && i != KeybordPlayerIndex)
+                    return false;
+            }
+
+            return true;
+        }
+
+        //プレイヤーデバイスクリア
+        public void ClearPlayerDevice()
+        {
+            for (int i = 0; i < PlayerDevices.Length; i++)
+                PlayerDevices[i] = null;
+        }
+    }
+}
