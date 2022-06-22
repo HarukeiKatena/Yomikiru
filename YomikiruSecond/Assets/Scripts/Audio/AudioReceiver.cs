147a73e376cff4d9d21734f5f83305cfbeed4a9c
i-yuuki <i-yuuki@users.noreply.github.com>
2022-06-22 17:05:58 +0900

    音をいっぱい鳴らせるようにする

--- YomikiruSecond/Assets/Scripts/Audio/AudioReceiver.cs
+++ YomikiruSecond/Assets/Scripts/Audio/AudioReceiver.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UniRx;
 
 namespace Yomikiru.Audio
@@ -7,20 +8,39 @@ namespace Yomikiru.Audio
     {
 
         [SerializeField] private AudioChannel channel;
-        [SerializeField] private bool ignoreSameCue;
         [SerializeField] private bool allowLoop;
-        [SerializeField] private AudioSource source;
+        [SerializeField] private int sourceCount;
+        [SerializeField] private AudioSource sourcePrefab;
+
+        private readonly List<AudioSource> sources = new List<AudioSource>();
 
         private void Awake()
         {
+            sources.Clear();
+            for (int i = 0; i < sourceCount; i++)
+            {
+                sources.Add(Instantiate(sourcePrefab, transform));
+            }
             channel.OnRequest.Subscribe(cue =>
             {
-                if (ignoreSameCue && source.clip == cue.Clip) return;
+                var source = FindIdleSource();
+                if(!source) return;
+                // 3Dサウンドはここをいじれ
                 source.clip = cue.Clip;
                 source.loop = allowLoop && cue.Loop;
                 source.Play();
             });
-            channel.OnStopRequest.Subscribe(cue => { source.Stop(); });
+            channel.OnStopRequest.Subscribe(_ => { StopAll(); });
+        }
+
+        private AudioSource FindIdleSource()
+        {
+            return sources.Find(source => !source.isPlaying);
+        }
+
+        private void StopAll()
+        {
+            sources.ForEach(source => source.Stop());
         }
 
     }
