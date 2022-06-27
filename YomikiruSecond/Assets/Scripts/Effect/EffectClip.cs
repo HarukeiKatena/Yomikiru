bd7faf8ffdfe177db8fc202509496b63805d6b36
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-06-27 19:11:32 +0900

    エフェクトクリップ作成

--- YomikiruSecond/Assets/Scripts/Effect/EffectClip.cs
+++ YomikiruSecond/Assets/Scripts/Effect/EffectClip.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
-using Yomikiru.Utility;
+
 
 namespace Yomikiru.Effect
 {
@@ -12,6 +11,11 @@ namespace Yomikiru.Effect
         [field: SerializeField] public int PlayMax { get; private set; }
         [field: SerializeField] public int LifeTime { get; private set; }
         [field: SerializeField] public bool isDynamic { get; private set; }
-        [field: SerializeField]public TransformationData Transformation { get; private set; }
+
+        [field: Header("Transformation")]
+        [field: SerializeField] public Vector3 StartScale { get; private set; } = new Vector3(0.1f, 0.1f, 0.1f);
+        [field: SerializeField] public Vector3 EndScale { get; private set; } = new Vector3(50.0f, 50.0f, 50.0f);
+        [field: SerializeField] public float Time { get; private set; } = 4.0f;
+        [field: SerializeField] public float Delay { get; private set; } = 0.0f;
     }
 }
