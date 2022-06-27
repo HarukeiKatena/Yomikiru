1c1793d8d04c241491e7c21f55053510cb761bf3
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-06-28 00:55:56 +0900

    テスト修正

--- YomikiruSecond/Assets/Scripts/Effect/EffectTest.cs
+++ YomikiruSecond/Assets/Scripts/Effect/EffectTest.cs
@@ -1,16 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using Yomikiru.Effect;
 
 public class EffectTest : MonoBehaviour
 {
+    //チャンネルと使いたいエフェクトのクリップを持っておく
     [SerializeField] private EffectChannel channel;
     [SerializeField] private EffectClip clip;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        channel.Request("", clip);
+        if (Keyboard.current.pKey.wasPressedThisFrame)
+        {
+            //Requestで再生できる(第一引数は自分のオブジェクト名を入れる、第二引数にはクリップ)
+            channel.Request("", clip);
+        }
     }
 }
