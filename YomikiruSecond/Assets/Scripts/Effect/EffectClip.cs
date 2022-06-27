e197c27d34e100bbc684042a9659bb2fc2a6553d
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-06-27 21:22:47 +0900

    エフェクトクリップに情報を追加

--- YomikiruSecond/Assets/Scripts/Effect/EffectClip.cs
+++ YomikiruSecond/Assets/Scripts/Effect/EffectClip.cs
@@ -7,14 +7,21 @@ namespace Yomikiru.Effect
     [CreateAssetMenu(menuName = "ScriptableObjects/EffectClip")]
     public class EffectClip : ScriptableObject
     {
-        [field: SerializeField] public GameObject EffectPrefub { get; private set; }
+        [field: SerializeField] public Mesh ObjectMesh { get; private set; }
+        [field: SerializeField] public Material EffectMaterial { get; private set; }
         [field: SerializeField] public int PlayMax { get; private set; }
         [field: SerializeField] public int LifeTime { get; private set; }
         [field: SerializeField] public bool isDynamic { get; private set; }
 
-        [field: Header("Transformation")]
-        [field: SerializeField] public Vector3 StartScale { get; private set; } = new Vector3(0.1f, 0.1f, 0.1f);
+        [field: Header("StartPosture")]
+        [field: SerializeField] public Vector3 BacePosition { get; private set; } = Vector3.zero;
+        [field: SerializeField] public Quaternion BaceRotation { get; private set; } = Quaternion.identity;
+
+        [field: Header("Scale")]
+        [field: SerializeField] public Vector3 StartScale { get; private set; } = Vector3.zero;
         [field: SerializeField] public Vector3 EndScale { get; private set; } = new Vector3(50.0f, 50.0f, 50.0f);
+
+        [field: Header("Time")]
         [field: SerializeField] public float Time { get; private set; } = 4.0f;
         [field: SerializeField] public float Delay { get; private set; } = 0.0f;
     }
