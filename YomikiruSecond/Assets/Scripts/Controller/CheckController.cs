3aa3f03ccdced4466c3719485b5f0ef4e1c9daf3
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-07-10 14:46:34 +0900

    チェックコントローラーの入力をインスペクター側から操作できるように修正

--- YomikiruSecond/Assets/Scripts/Controller/CheckController.cs
+++ YomikiruSecond/Assets/Scripts/Controller/CheckController.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Linq;
 using UniRx;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 namespace Yomikiru.Controller
 {
@@ -9,10 +11,98 @@ namespace Yomikiru.Controller
     {
         [SerializeField] private ControllerManager controller;
 
+        [Header("JoinButton")]
+        [SerializeField] private InputAction joinButton;
+
+        [Header("ExitButton")]
+        [SerializeField] private InputAction cancelButton;
+
         public ControllerManager ControllerManager => controller;
         public IObservable<ControllerManager> ChangePlayerDevice => changePlayerDevice;
         private Subject<ControllerManager> changePlayerDevice = new Subject<ControllerManager>();
 
+        private void Start()
+        {
+            joinButton.performed += CheckInputDeviceEvent;
+            cancelButton.performed += CancelPlayerDeviceEvent;
+        }
+
+        private void CheckInputDeviceEvent(InputAction.CallbackContext callback)
+        {
+            var device = callback.control.device;
+
+            //入力されたデバイスがゲームパッドの場合
+            foreach (var gamepad in Gamepad.all.Where(gamepad => gamepad.deviceId == device.deviceId))
+            {
+                for (int i = 0; i < controller.PlayerCount; i++)
+                {
+                    //同じやつがある場合抜ける
+                    if(controller.PlayerDevices[i] == gamepad)
+                        break;
+
+                    //使われている場合抜ける
+                    if (controller.KeybordPlayerIndex == i ||
+                        controller.PlayerDevices[i] != null)
+                        continue;
+
+                    controller.PlayerDevices[i] = gamepad;
+                    changePlayerDevice.OnNext(controller);//イベント通知
+                    break;
+                }
+            }
+
+            //入力されたデバイスがキーボードの場合
+            if (Keyboard.current.deviceId == device.deviceId)
+            {
+                for (int i = 0; i < controller.PlayerCount; i++)
+                {
+                    //未登録の場所にセットする
+                    if (controller.PlayerDevices[i] != null)
+                        continue;
+
+                    controller.KeybordPlayerIndex = i;
+                    changePlayerDevice.OnNext(controller);
+                    break;
+                }
+            }
+        }
+
+        private void CancelPlayerDeviceEvent(InputAction.CallbackContext callback)
+        {
+            var device = callback.control.device;
+            for (int i = 0; i < controller.PlayerCount; i++)
+            {
+                //パッド
+                if (controller.PlayerDevices[i] != null &&
+                    device.deviceId == controller.PlayerDevices[i].deviceId)
+                {
+                    controller.PlayerDevices[i] = null;
+                    changePlayerDevice.OnNext(controller);//イベント通知
+                }
+
+                //キーボード
+                if (controller.KeybordPlayerIndex == i &&
+                    device.deviceId == Keyboard.current.deviceId)
+                {
+                    controller.KeybordPlayerIndex = ControllerManager.NotUsedKeybord;
+                    changePlayerDevice.OnNext(controller);
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            joinButton.Enable();
+            cancelButton.Enable();
+        }
+
+        private void OnDisable()
+        {
+            joinButton.Disable();
+            cancelButton.Disable();
+        }
+
+        /*//何かあったとき用に残しておく
         private void Update()
         {
             CheckInputDevice();
@@ -84,6 +174,6 @@ namespace Yomikiru.Controller
                 }
             }
         }
-
+        */
     }
 }
