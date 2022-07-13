2b506e9943b2bc11697c5e56029c6dc78231793e
harukei <58187672+hakiyoshi@users.noreply.github.com>
2022-07-13 20:51:56 +0900

    DollyCartをUniTaskで動かすように修正 カメラ移動が終わったらイベントを発行するやつ追加

--- YomikiruSecond/Assets/Scripts/Result/ResultSequence.cs
+++ YomikiruSecond/Assets/Scripts/Result/ResultSequence.cs
@@ -1,16 +1,44 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Security;
+using System.Threading;
+using Cinemachine;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UniRx;
 using UnityEngine;
-using Yomikiru;
 
-public class ResultSequence : MonoBehaviour
+namespace Yomikiru.Result
 {
-    [SerializeField] private Animator playerAnimator;
-
-    private void Start()
+    public class ResultSequence : MonoBehaviour
     {
-        playerAnimator.SetBool("isWin", true);
+        [SerializeField] private Animator playerAnimator;
+        [SerializeField] private CinemachineDollyCart dollyCart;
+        [SerializeField] private float cameraMoveTime = 20.0f;
+
+        public IObservable<Unit> OnStopCamera => onStopCamera;
+        private Subject<Unit> onStopCamera = new Subject<Unit>();
+
+        private void Start()
+        {
+            playerAnimator.SetBool("isWin", true);
+
+            Result(this.GetCancellationTokenOnDestroy()).Forget();
+
+            OnStopCamera.Subscribe(_ =>
+            {
+                Debug.Log("カメラ止まったよ");
+            });
+        }
+
+        private async UniTask Result(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+
+            //カメラ移動
+            await DOVirtual.Float(0.0f, 1.0f, cameraMoveTime, x => dollyCart.m_Position = x).SetEase(Ease.OutCubic)
+                .ToUniTask(cancellationToken: token);
+
+            onStopCamera.OnNext(Unit.Default);
+            onStopCamera.OnCompleted();
+        }
     }
 }
